eefb95e6ee9892f6c3b027c61464de8519b12356
Alibahrehmand <116271525+Alibahrehmand@users.noreply.github.com>
2026-05-30 15:50:57 +0200

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+print("CyberGuard AI")
 
+question = input("Ask a cybersecurity question: ")
+
+if "phishing" in question.lower():
+    print("Phishing is a social engineering attack used to steal sensitive information.")
+elif "ransomware" in question.lower():
+    print("Ransomware encrypts files and demands payment.")
+else:
+    print("I don't know that answer yet.")
